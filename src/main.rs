b4d43d67a566d156bcdf46ecefe2f590309c6ce4
branistecristian <cristianbraniste964@gmail.com>
2025-05-10 00:51:51 +0300

    Testing: Display functionality

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,73 @@
 #![no_std]
 #![no_main]
 
+use {defmt_rtt as _, panic_probe as _}; // for logging + panic handling
+
 use embassy_executor::Spawner;
-use embassy_net::StackResources;
-use embassy_time::{Duration, Timer};
-use static_cell::StaticCell;
-use {defmt_rtt as _, panic_probe as _};
+use embassy_rp::i2c::{self, I2c, InterruptHandler as I2CInterruptHandler, Config as I2cConfig};
+use embassy_rp::peripherals::{PIN_10, PIN_11};
+use embassy_time::{Timer, Duration};
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::Text,
+};
+use ssd1306::{
+    prelude::*,
+    I2CDisplayInterface,
+    Ssd1306,
+    mode::BufferedGraphicsMode,
+    rotation::DisplayRotation,
+};
 
 // Use the logging macros provided by defmt.
 use defmt::*;
 
-// Import interrupts definition module
-mod irqs;
+use embassy_rp::bind_interrupts;
 
-const SOCK: usize = 4;
-static RESOURCES: StaticCell<StackResources<SOCK>> = StaticCell::<StackResources<SOCK>>::new();
+use embedded_hal_async::i2c::{Error, I2c as _};
+use embassy_rp::peripherals::I2C0;
+
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    // Get a handle to the RP's peripherals.
+    // Initialize the embassy runtime and peripherals.
     let peripherals = embassy_rp::init(Default::default());
 
-    // Init WiFi driver
-    let (net_device, mut _control) = embassy_lab_utils::init_wifi!(&spawner, peripherals).await;
+    bind_interrupts!(struct Irqs {
+        I2C0_IRQ => i2c::InterruptHandler<I2C0>;
+    });
 
-    // Default config for dynamic IP address
-    let config = embassy_net::Config::dhcpv4(Default::default());
-
-    // Init network stack
-    let _stack = embassy_lab_utils::init_network_stack(&spawner, net_device, &RESOURCES, config);
+    let i2c = I2c::new_async(
+        peripherals.I2C0,     // I2C0 
+        peripherals.PIN_5,    // SCL = GP5
+        peripherals.PIN_4,    // SDA = GP4
+        Irqs,                 // <- this is important for async!
+        I2cConfig::default(),
+    );
 
     info!("Hello world!");
 
-    let delay = Duration::from_secs(1);
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display: Ssd1306<_, _, BufferedGraphicsMode<_>> = Ssd1306::new(
+        interface,
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    )
+    .into_buffered_graphics_mode();
+
+    display.init().unwrap();
+    display.flush().unwrap();
+
+    let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+    Text::new("Hello :)", Point::new(32, 32), text_style)
+        .draw(&mut display)
+        .unwrap();
+    display.flush().unwrap();
+
     loop {
-        Timer::after(delay).await;
+        Timer::after(Duration::from_secs(1)).await;
     }
 }
